472f94a3d10fc5258faa16f571dad8e5454b884e
Roberto E. B. Junior <robertoeb.jr@gmail.com>
2019-03-09 11:14:44 -0300

    Update day 2 exponent.rb

--- day 2/exponent.rb
+++ day 2/exponent.rb
@@ -1,14 +1,14 @@
-system "clear"
+system 'clear'
 
 def exponent_calc(base, exponent)
-  result = base ** exponent
+  result = base**exponent
   puts "\nThe result of the #{base} to #{exponent}nd power is: #{result}.\n\n"
 end
 
-print 'Enter the base: '
+print 'Enter the base number: '
 base = gets.chomp.to_i
 
-print "\nEnter the exponent: "
+print "\nEnter the exponent number: "
 exponent = gets.chomp.to_i
 
 exponent_calc(base, exponent)
